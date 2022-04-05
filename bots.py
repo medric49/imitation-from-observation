bc3f89a4a91489a12c8737a9779c01548a37f07d
Medric Sonwa <medric49@gmail.com>
2022-04-04 20:24:11 -0400

    Update move limits

--- bots.py
+++ bots.py
@@ -7,29 +7,34 @@ import numpy as np
 
 class ArmEnv(dm_env.Environment):
     def __init__(self, model_name='rx150'):
-        self.bot = InterbotixManipulatorXS(model_name, 'arm', 'gripper')
+        self._env = InterbotixManipulatorXS(model_name, 'arm', 'gripper')
 
     def reset(self) -> TimeStep:
-        np.random.uniform()
-        self.bot.arm.set_joint_positions(np.random.uniform([-np.pi/3, -np.pi/20, -np.pi/12, np.pi/3, 0.], [np.pi/3, 0., 0., np.pi/2, 0.]))
+        self._env.arm.set_ee_pose_components(
+            x=np.random.uniform(0.15, 0.25),
+            y=np.random.uniform(-0.25, 0.25),
+            z=.18,
+            pitch=np.pi/4
+        )
         return dm_env.TimeStep(step_type=dm_env.StepType.FIRST, reward=0., discount=1., observation=None)
 
     def step(self, action) -> TimeStep:
-        ac = np.zeros((5,))
-        ac[:4] = action
-        self.bot.arm.set_joint_positions(ac)
+        self._env.arm.set_ee_pose_components(x=action[0], y=action[1], z=action[2], pitch=np.pi / 4)
         return dm_env.TimeStep(step_type=dm_env.StepType.MID, reward=0., discount=1., observation=None)
 
     def get_random_action(self):
-        return np.random.uniform([-np.pi/3, -np.pi/20, -np.pi/12, np.pi/3], [np.pi/3, np.pi/20, np.pi/20, np.pi/2])
+        return np.random.uniform(self.action_spec().minimum, self.action_spec().maximum)
 
     def observation_spec(self):
         pass
 
     def action_spec(self):
         return BoundedArray(
-            (4,),
+            (3,),
             dtype=np.float32,
-            minimum=np.array([-np.pi/3, -np.pi/20, -np.pi/12, np.pi/3]),
-            maximum=np.array([np.pi/3, np.pi/20, np.pi/20, np.pi/2])
+            minimum=np.array([.12, -.25, .06]),
+            maximum=np.array([.25, .25, .18])
         )
+
+    def __getattr__(self, name):
+        return getattr(self._env, name)
