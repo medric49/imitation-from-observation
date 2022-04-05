02aee74d1977ac06a00bd7dd8e6a75917e927190
Medric Sonwa <medric49@gmail.com>
2022-04-04 20:54:00 -0400

    Update robot env code

--- bots.py
+++ bots.py
@@ -8,18 +8,17 @@ import numpy as np
 class ArmEnv(dm_env.Environment):
     def __init__(self, model_name='rx150'):
         self._env = InterbotixManipulatorXS(model_name, 'arm', 'gripper')
+        self._location = None
 
     def reset(self) -> TimeStep:
-        self._env.arm.set_ee_pose_components(
-            x=np.random.uniform(0.15, 0.25),
-            y=np.random.uniform(-0.25, 0.25),
-            z=.18,
-            pitch=np.pi/4
-        )
+        self._location = np.random.uniform([.15, -.25, .18], [.25, .25, .18])
+        self._env.arm.set_ee_pose_components(x=self._location[0], y=self._location[1], z=self._location[2], pitch=np.pi/4, moving_time=0.7)
         return dm_env.TimeStep(step_type=dm_env.StepType.FIRST, reward=0., discount=1., observation=None)
 
     def step(self, action) -> TimeStep:
-        self._env.arm.set_ee_pose_components(x=action[0], y=action[1], z=action[2], pitch=np.pi / 4)
+        self._location += action
+        self._location = np.clip(self._location, np.array([.12, -.25, .06]), np.array([.25, .25, .18]))
+        self._env.arm.set_ee_pose_components(x=self._location[0], y=self._location[1], z=self._location[2], pitch=np.pi/4, moving_time=0.25)
         return dm_env.TimeStep(step_type=dm_env.StepType.MID, reward=0., discount=1., observation=None)
 
     def get_random_action(self):
@@ -32,8 +31,8 @@ class ArmEnv(dm_env.Environment):
         return BoundedArray(
             (3,),
             dtype=np.float32,
-            minimum=np.array([.12, -.25, .06]),
-            maximum=np.array([.25, .25, .18])
+            minimum=np.array([-.02, -.02, -.02]),
+            maximum=np.array([.02, .02, .02])
         )
 
     def __getattr__(self, name):
