13a22c86f0f2a6fbc5d2ae102b38558dd1256e51
Medric Sonwa <medric49@gmail.com>
2022-04-23 01:55:51 -0400

    update

--- dmc.py
+++ dmc.py
@@ -239,7 +239,7 @@ class EncodeStackWrapper(dm_env.Environment):
             s1 = state[-self.state_dim:]
             s2 = self.avg_states[self.step_id]
             # reward = -np.linalg.norm(s1 - s2)
-            reward = max(dot(s1, s2) / (norm(s1) * norm(s2)), 0)
+            reward = dot(s1, s2) / (norm(s1) * norm(s2))
         else:
             reward = time_step.reward
 
@@ -366,7 +366,7 @@ class CTStackWrapper(dm_env.Environment):
                 obs = torch.tensor(time_step.observation[-self.init_channel:], dtype=torch.float, device=utils.device()).unsqueeze(0)
                 s1 = self.context_translator.encode(obs)[0].cpu().numpy()
             s2 = self.avg_states[self.step_id]
-            reward = max(dot(s1, s2) / (norm(s1) * norm(s2)), 0)
+            reward = dot(s1, s2) / (norm(s1) * norm(s2))
         else:
             reward = time_step.reward
 
