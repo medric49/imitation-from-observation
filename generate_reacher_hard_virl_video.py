befd6ed5c2dcde8453f752a1e9445965ff48e25e
Medric Sonwa <medric49@gmail.com>
2022-05-31 20:50:57 -0400

    generate video script

--- generate_reacher_hard_virl_video.py
+++ generate_reacher_hard_virl_video.py
@@ -5,6 +5,7 @@ import context_changers
 import dmc
 import utils
 from drqv2 import DrQV2Agent
+from pathlib import Path
 
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 
@@ -16,28 +17,30 @@ if __name__ == '__main__':
 
     ep_len = 30
 
+    video_dir = Path('videos/reacher_hard_virl_v2')
+
     env = dmc.make('reacher_hard', frame_stack=3, action_repeat=2, seed=2, xml_path='domain_xmls/reacher_2_targets.xml')
     context_changer = context_changers.ReacherHardTargetSwitcherContextChanger()
     utils.generate_video_from_expert(
-        'videos/reacher_hard_virl/train/1', expert, env, context_changer, cam_ids=[0],
+        video_dir / 'train/1', expert, env, context_changer, cam_ids=[0],
         num_frames=ep_len, num_train=num_train)
 
     env = dmc.make('reacher_hard', frame_stack=3, action_repeat=2, seed=2, xml_path='domain_xmls/reacher.xml')
     context_changer = context_changers.ReacherHardContextChanger()
     utils.generate_video_from_expert(
-        'videos/reacher_hard_virl/train/0', expert, env, context_changer, cam_ids=[0],
+        video_dir / 'train/0', expert, env, context_changer, cam_ids=[0],
         num_frames=ep_len, num_train=num_train)
 
     env = dmc.make('reacher_hard', frame_stack=3, action_repeat=2, seed=2, xml_path='domain_xmls/reacher_2_targets.xml')
     context_changer = context_changers.ReacherHardTargetSwitcherContextChanger()
     utils.generate_video_from_expert(
-        'videos/reacher_hard_virl/valid/1', expert, env, context_changer, cam_ids=[0],
+        video_dir / 'valid/1', expert, env, context_changer, cam_ids=[0],
         num_frames=ep_len, num_train=num_valid)
 
     env = dmc.make('reacher_hard', frame_stack=3, action_repeat=2, seed=2, xml_path='domain_xmls/reacher.xml')
     context_changer = context_changers.ReacherHardContextChanger()
     utils.generate_video_from_expert(
-        'videos/reacher_hard_virl/valid/0', expert, env, context_changer, cam_ids=[0],
+        video_dir / 'valid/0', expert, env, context_changer, cam_ids=[0],
         num_frames=ep_len, num_train=num_valid)
 
 
