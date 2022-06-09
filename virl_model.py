1714a4875a8a0fc91dcf31e28ee2da0b65d04b00
Medric Sonwa <medric49@gmail.com>
2022-06-08 20:13:49 -0400

    update virl model

--- virl_model.py
+++ virl_model.py
@@ -203,7 +203,7 @@ class ViRLNet(nn.Module):
 class ConvNet(nn.Module):
     def __init__(self, hidden_dim):
         super(ConvNet, self).__init__()
-        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
+        self.leaky_relu = nn.LeakyReLU()
         self.sigmoid = nn.Sigmoid()
         self.flatten = nn.Flatten()
         self.conv_1 = nn.Conv2d(3, 64, kernel_size=5, stride=2)
@@ -214,9 +214,9 @@ class ConvNet(nn.Module):
         self.b_norm_3 = nn.BatchNorm2d(256)
         self.conv_4 = nn.Conv2d(256, 512, kernel_size=5, stride=2)
         self.b_norm_4 = nn.BatchNorm2d(512)
-        self.fc1 = nn.Conv2d(512, hidden_dim, kernel_size=1)
-        self.b_norm_fc_1 = nn.BatchNorm2d(hidden_dim)
-        self.fc2 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1)
+        self.fc1 = nn.Conv2d(512, hidden_dim * 4, kernel_size=1)
+        self.b_norm_fc_1 = nn.BatchNorm2d(hidden_dim * 4)
+        self.fc2 = nn.Conv2d(hidden_dim * 4, hidden_dim, kernel_size=1)
 
     def forward(self, obs):
         c1 = self.leaky_relu(self.b_norm_1(self.conv_1(obs)))
@@ -232,74 +232,82 @@ class ConvNet(nn.Module):
 class DeconvNet(nn.Module):
     def __init__(self, hidden_dim):
         super(DeconvNet, self).__init__()
-        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
+        self.leaky_relu = nn.LeakyReLU()
 
-        self.fc = nn.Conv2d(hidden_dim, 512, kernel_size=1)
-        self.b_norm_fc = nn.BatchNorm2d(512)
-        self.conn_4 = nn.Conv2d(512 * 2, 512, kernel_size=1, stride=1)
+        self.fc2 = nn.Conv2d(hidden_dim, hidden_dim * 4, kernel_size=1)
+        self.fc1 = nn.Conv2d(hidden_dim * 4, 512, kernel_size=1)
+        self.b_norm_fc_1 = nn.BatchNorm2d(512)
+        # self.conn_4 = nn.Conv2d(512 * 2, 512, kernel_size=1, stride=1)
 
         self.t_conv_4 = nn.ConvTranspose2d(512, 256, kernel_size=5, stride=2)
         self.b_norm_4 = nn.BatchNorm2d(256)
-        self.conn_3 = nn.Conv2d(256 * 2, 256, kernel_size=1, stride=1)
+        # self.conn_3 = nn.Conv2d(256 * 2, 256, kernel_size=1, stride=1)
 
         self.t_conv_3 = nn.ConvTranspose2d(256, 128, kernel_size=5, stride=2)
         self.b_norm_3 = nn.BatchNorm2d(128)
-        self.conn_2 = nn.Conv2d(128 * 2, 128, kernel_size=1, stride=1)
+        # self.conn_2 = nn.Conv2d(128 * 2, 128, kernel_size=1, stride=1)
 
         self.t_conv_2 = nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2, output_padding=1)
         self.b_norm_2 = nn.BatchNorm2d(64)
-        self.conn_1 = nn.Conv2d(64 * 2, 64, kernel_size=1, stride=1)
+        # self.conn_1 = nn.Conv2d(64 * 2, 64, kernel_size=1, stride=1)
 
         self.t_conv_1 = nn.ConvTranspose2d(64, 3, kernel_size=5, stride=2, output_padding=1)
 
     def forward(self, e, c1, c2, c3, c4):
         e = e.view(e.shape[0], e.shape[1], 1, 1)
-        d4 = self.leaky_relu(self.b_norm_fc(self.fc(e)))
-        d4 = self.leaky_relu(self.conn_4(torch.cat([c4, d4], dim=1)))
+        e = self.leaky_relu(self.fc2(e))
+
+        d4 = self.leaky_relu(self.b_norm_fc_1(self.fc1(e)))
+        # d4 = self.leaky_relu(self.conn_4(torch.cat([c4, d4], dim=1)))
 
         d3 = self.leaky_relu(self.b_norm_4(self.t_conv_4(d4)))
-        d3 = self.leaky_relu(self.conn_3(torch.cat([c3, d3], dim=1)))
+        # d3 = self.leaky_relu(self.conn_3(torch.cat([c3, d3], dim=1)))
 
         d2 = self.leaky_relu(self.b_norm_3(self.t_conv_3(d3)))
-        d2 = self.leaky_relu(self.conn_2(torch.cat([c2, d2], dim=1)))
+        # d2 = self.leaky_relu(self.conn_2(torch.cat([c2, d2], dim=1)))
 
         d1 = self.leaky_relu(self.b_norm_2(self.t_conv_2(d2)))
-        d1 = self.leaky_relu(self.conn_1(torch.cat([c1, d1], dim=1)))
+        # d1 = self.leaky_relu(self.conn_1(torch.cat([c1, d1], dim=1)))
 
         obs = self.leaky_relu(self.t_conv_1(d1))
         return obs
 
 
 class LSTMEncoder(nn.Module):
-    def __init__(self, hidden_dim):
+    def __init__(self, input_size):
         super(LSTMEncoder, self).__init__()
         self.num_layers = 2
-        self.encoder = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, num_layers=self.num_layers)
+        self.encoder = nn.LSTM(input_size=input_size, hidden_size=input_size * 2, num_layers=self.num_layers)
+        self.fc = nn.Linear(input_size * 2, input_size)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, e_seq):
-        h0 = torch.zeros(e_seq.shape[1:]).repeat(self.num_layers, 1, 1).to(device=e_seq.device)
-        c0 = torch.zeros(e_seq.shape[1:]).repeat(self.num_layers, 1, 1).to(device=e_seq.device)
-
-        h_seq, hidden = self.encoder(e_seq, (h0, c0))
+        h_seq, hidden = self.encoder(e_seq)
         h = h_seq[-1]
+        h = self.sigmoid(self.fc(h))
         return h, hidden
 
 
 class LSTMDecoder(nn.Module):
-    def __init__(self, hidden_dim):
+    def __init__(self, input_size):
         super(LSTMDecoder, self).__init__()
         self.num_layers = 2
-        self.decoder = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, num_layers=self.num_layers)
+        self.decoder = nn.LSTM(input_size=input_size * 2, hidden_size=input_size, num_layers=self.num_layers)
+        self.fc = nn.Linear(input_size, input_size * 2)
 
     def forward(self, h, hidden, T):
-        # h0 = torch.zeros(h.shape).repeat(self.num_layers, 1, 1).to(device=h.device)
-        # c0 = torch.zeros(h.shape).repeat(self.num_layers, 1, 1).to(device=h.device)
-        # hidden = (h0, c0)
-        h = h.unsqueeze(0)
+        hidden = None
         e_seq = []
         for _ in range(T):
+            h = self.fc(h)
+            h = h.unsqueeze(0)
+
             h, hidden = self.decoder(h, hidden)
-            e_seq.append(h[0])
+
+            h = h.squeeze(0)
+            e_seq.append(h)
+
+
 
         e_seq = torch.stack(e_seq)
         return e_seq
