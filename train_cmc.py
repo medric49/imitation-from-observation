8e4c53d00f8d04febf2872bfa0eefbfa434cf5f0
Medric Sonwa <medric49@gmail.com>
2022-06-24 21:07:32 -0400

    fix cmc

--- train_cmc.py
+++ train_cmc.py
@@ -39,7 +39,7 @@ class Workspace:
 
         self.cfg = cfg
         utils.set_seed_everywhere(cfg.seed)
-        self.encoder: cmc_model.CMCModel = hydra.utils.instantiate(self.cfg.virl_model).to(utils.device())
+        self.encoder: cmc_model.CMCModel = hydra.utils.instantiate(self.cfg.cmc_model).to(utils.device())
 
         self.dataset = datasets.ViRLVideoDataset(to_absolute_path(self.cfg.train_video_dir), self.cfg.episode_len, self.cfg.train_cams, to_lab=True)
         self.valid_dataset = datasets.ViRLVideoDataset(to_absolute_path(self.cfg.valid_video_dir), self.cfg.episode_len, self.cfg.train_cams, to_lab=True)
